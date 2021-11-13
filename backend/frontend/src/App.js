afa12d73a025aa2af028fe5c725ecc57b9d04293
Tejas Chaplot <tejas.chaplot@somaiya.edu>
2021-11-13 08:34:21 +0530

    frontend is under backend

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import HomeScreen from './Screens/HomeScreen'
 import ProductScreen from './Screens/ProductScreen'
 import CartScreen from './Screens/CartScreen'
